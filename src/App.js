1606a16ee1058c99335c3e80e11811c5356cc2e7
Margot <mnoliverdesign@gmail.com>
2021-05-18 15:14:59 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [weather, setWeather] = useState("");
+  const [temperature, setTemperature] = useState(0);
+  const [cityName, setCityName] = useState("");
+
+  const savePositionToState = (position) => {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+  };
+
+  const fetchWeather = async () => {
+    try {
+      await window.navigator.geolocation.getCurrentPosition(savePositionToState);
+      const res = await axios.get(
+        `https.api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitudeappid=59cc34f169f6d5788ffd70587fbdb24c`
+      );
+      setTemperature(res.data.main.temp);
+      setCityName(res.data.name);
+      setWeather(res.data.weather[0].main);
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather();    
+  }, [latitude, longitude]);
+
+  return ( 
+  <div className='app'>
+    <div className ='app__container'>
+      <h1>{cityName}</h1>
+      <h2>{temperature}°C</h2>
+      <h2>{weather}</h2>
     </div>
+  </div>
   );
 }
 
